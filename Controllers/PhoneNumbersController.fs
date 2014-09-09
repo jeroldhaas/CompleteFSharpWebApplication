58e6574a636143553ca785349aac9c043c995c77
Jerold Haas <JeroldHaas@gmail.com>
2014-09-09 02:35:34 -0400

    upload changes to server

---
+++ Controllers/PhoneNumbersController.fs
@@ -0,0 +1,12 @@
+﻿namespace CompleteFSharpWebApplication.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Web
+open System.Web.Mvc
+open System.Web.Mvc.Ajax
+
+type PhoneNumbersController() =
+    inherit Controller()
+    member this.Index () = this.View()
